fe2bfee0a5c73625f68032739ddac4bc8716135f
Magkinney21 <magkinney21@gmail.com>
2017-03-08 16:36:28 -0500

    bootstraped modals

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//$('#button').on.click( handler );
+//$( "#1r1" ).append $( "#myModal" );
+//$( "#1r1" ).append( $( "#myModal");
+
+var container2 = $("button".on('click', function() {
+    display(container2);
+});
