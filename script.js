85c3ebf432f43d2928d6fa5374ceb60106217e18
Magkinney21 <magkinney21@gmail.com>
2017-03-10 12:55:53 -0500

    done

--- script.js
+++ script.js
@@ -1,11 +1,158 @@
-// $( document ).ready(function(){
- // var startGame = promt("would you like to play")
- // if (startGame === "yes"){
-  // alert("Began by Clicking a button with a $");
- // } else{
-  // alert ("please exit game");
- // };
+var points = 0;
+$('zero').onclick = reset;
+$('#animal100C').click(function(){
+    points += 100;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#art100D').click(function(){
+    points += 100;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#music100A').click(function(){
+    points += 100;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#space100C').click(function(){
+    points += 100;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#book100B').click(function(){
+    points += 100;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#animal200A').click(function(){
+    points += 200;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#art200A').click(function(){
+    points += 200;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#music200').click(function(){
+    points += 200;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#space200B').click(function(){
+    points += 200;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#book200A').click(function(){
+    points += 200;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#animal300B').click(function(){
+    points += 300;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#art300A').click(function(){
+    points += 300;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#music300D').click(function(){
+    points += 300;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#space300D').click(function(){
+    points += 300;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#book300C').click(function(){
+    points += 300;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#animal400A').click(function(){
+    points += 400;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#art400C').click(function(){
+    points += 400;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#music400B').click(function(){
+    points += 400;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#space400C').click(function(){
+    points += 400;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#book400A').click(function(){
+    points += 400;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#animal500B').click(function(){
+    points += 500;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#art500C').click(function(){
+    points += 500;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#music500A').click(function(){
+    points += 500;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#space500B').click(function(){
+    points += 500;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+$('#book500B').click(function(){
+    points += 500;
+  $('#number').text(points);
+  console.log("points " + points )
+});
+//////reset button
+function reset(points) {
+  points = 0;
+  $('#number').text(points);
+}
+//////correct functions
+function add100() {
 
+}
+function add200() {
+  points = points + 200;
+  $('#number').text(points);
+}
+function add300() {
+  points = points + 300;
+  $('#number').text(points);
+}
+function add400() {
+  points = points + 400;
+  $('#number').text(points);
+}
+///////incorrect functions
+function add500() {
+  points = points + 500;
+  $('#number').text(points);
+}
+
+//answers below this line //
 $( document ).ready(function() {
   var correctAnswer = "animal100C";
   $(".animal100").on("click" , function(){
