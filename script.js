0fe9cb830b76d6d8733eea928fe0d212bb72a8b6
Magkinney21 <magkinney21@gmail.com>
2017-03-09 19:03:21 -0500

    yes js yes

--- script.js
+++ script.js
@@ -1,13 +1,326 @@
+
 $( document ).ready(function() {
-  var correctAnswer = "C";
-  $("#animalC").on("click", function(){
-    var answer = "C"
-    if (answer === correctAnswer ) {
-      alert("correct!");
-    } else {
-      alert("incorrect");
-    };
+  var correctAnswer = "animal100C";
+  $(".animal100").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    //console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    //console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
   });
 });
-
-
+$( document ).ready(function() {
+  var correctAnswer = "art100D";
+  $(".art100").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    //console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    //console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "music100A";
+  $(".music100").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "space100C";
+  $(".space100").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "book100B";
+  $(".book100").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "animal200A";
+  $(".animal200").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "art200A";
+  $(".art200").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "music200A";
+  $(".music200").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "space200B";
+  $(".space200").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "book200A";
+  $(".book200").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "animal300B";
+  $(".animal300").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "art300A";
+  $(".art300").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "music300D";
+  $(".music300").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "space300D";
+  $(".space300").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "book300C";
+  $(".book300").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "animal400A";
+  $(".animal400").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "art400C";
+  $(".art400").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "music400B";
+  $(".music400").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "space400C";
+  $(".space400").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "book400A";
+  $(".book400").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "animal500B";
+  $(".animal500").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "art500C";
+  $(".art500").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "music500A";
+  $(".music500").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+});
+$( document ).ready(function() {
+  var correctAnswer = "space500B";
+  $(".space500").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+})
+$( document ).ready(function() {
+  var correctAnswer = "book500B";
+  $(".book500").on("click" , function(){
+   if(this.id === correctAnswer){
+    alert("correct");
+    console.log('correct answer, should be alert');
+   }else{
+    alert("incorrect");
+    console.log('incorrect answer, should be alert');
+   }
+   console.log(this.id);
+  });
+})
